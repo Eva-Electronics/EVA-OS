f4fdaf95785902046a41089dc2217abfac2c30c8
Elmar <Eva.Electronics@outlook.com>
2013-08-03 05:20:53 +0200

    Initial commit
    Signed-off-by: Elmar <Eva.Electronics@outlook.com>

---
+++ Drivers/Drivers.c
@@ -0,0 +1,24 @@
+/*
+ * Drivers.c
+ *
+ * Created: 8/3/2013 5:20:00 AM
+ *  Author: Elmar
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
